96214638cc89bc105f0e8692775b8658ac6383f9
Andy <andyalcivarj@gmail.com>
2026-01-09 19:50:47 -0500

    Configure for Render deployment

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -85,3 +85,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
